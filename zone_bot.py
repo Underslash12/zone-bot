e1709f275f1a498b9ea32b89df5b97748757e712
Underslash12 <stew_smith@hotmail.com>
2023-08-24 15:13:06 -0700

    Added Co-op Zone and No Zone features

--- zone_bot.py
+++ zone_bot.py
@@ -16,23 +16,24 @@ guild: discord.Guild = None
 zones: list[discord.VoiceChannel] = None
 warp_zones: dict[str, discord.VoiceChannel] = None
 
+# set the globals that contain the various zones
 @client.event
 async def on_ready():
     global guild, zones, warp_zones
-
-    print(f'We have logged in as {client.user}')
     
     guild = client.get_guild(SERVER_ID)
     zones = await get_zones()
     warp_zones = await get_warp_zones()
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    print(f"Logged in as {client.user}")
 
-    if message.content.startswith('ping'):
-        await message.channel.send('pong')
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+
+#     if message.content.startswith('ping'):
+#         await message.channel.send('pong')
 
 # get a list of the warp zones as VoiceChannel objects
 async def get_warp_zones() -> dict[str, discord.VoiceChannel]:
@@ -56,7 +57,7 @@ async def get_zones() -> list[discord.VoiceChannel]:
 @client.event
 async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
     # return if user leaves call
-    if after == None:
+    if after == None or after.channel == None:
         return
     
     # verify user joined a warp zone
@@ -72,9 +73,9 @@ async def on_voice_state_update(member: discord.Member, before: discord.VoiceSta
     elif after.channel == warp_zones["The Party Zone"]:
         await on_join_party_zone(member, before, after)
     elif after.channel == warp_zones["The Co-Op Zone"]:
-        pass
+        await on_join_coop_zone(member, before, after)
     elif after.channel == warp_zones["The No Zone"]:
-        pass
+        await on_join_no_zone(member, before, after)
     elif after.channel == warp_zones["The David Zone"]:
         await on_join_david_zone(member, before, after)
     else:
@@ -86,23 +87,49 @@ async def on_join_roll_the_zone(member: discord.Member, before: discord.VoiceSta
     await member.move_to(random_zone)
 
 # sends the user to the first most populated zone
+# if no zone is populated, send the user to the no zone
 async def on_join_party_zone(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
     largest_zone = zones[0]
     for zone in zones:
         if len(zone.members) > len(largest_zone.members):
             largest_zone = zone
-    await member.move_to(largest_zone)
+
+    if len(largest_zone.members) > 0:
+        await member.move_to(largest_zone)
+    else:
+        await member.move_to(warp_zones["The No Zone"])
+
+# sends the user to a zone with 1 person in it
+# if there are no zones with a single person in them, send the user to the no zone
+async def on_join_coop_zone(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+    for zone in zones:
+        if len(zone.members) == 1:
+            await member.move_to(zone)
+            return
+    else:
+        await member.move_to(warp_zones["The No Zone"])
+
+# sends the user to a random zone that isn't populated
+async def on_join_no_zone(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+    no_zones = list(filter(lambda z: len(z.members) == 0, zones))
+    random_no_zone = random.choice(no_zones)
+    await member.move_to(random_no_zone)
 
 # sends the users down the zones 1 by 1 until they hit a populated zone
 # rate limit is about 1/sec and it takes about 0.2-0.3 secs to move, so 0.8 doens't really rate limit
 async def on_join_david_zone(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
-    for i in range(len(zones)):
-        time.sleep(0.8) # get second opinion on initial pause or not
-        if len(zones[i].members) > 0:
-            await member.move_to(zones[i])
-            break
-        else:
-            await member.move_to(zones[i])
+    # stop if the user disconnects in the middle of the move
+    try:
+        for i in range(len(zones)):
+            time.sleep(0.8) # get second opinion on initial pause or not
+            if len(zones[i].members) > 0:
+                await member.move_to(zones[i])
+                break
+            else:
+                await member.move_to(zones[i])
+    except discord.errors.HTTPException as e:
+        pass
 
+# run the bot with the token specified in token.txt
 with open("token.txt", "r") as token_file:
     client.run(token_file.read())
